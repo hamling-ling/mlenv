bcc5aaacf9a9fabe6ba05c79ad2f4aa63857c12d
hamling-ling <nobubijinesu@gmail.com>
2019-01-10 23:52:30 +0900

    run docker as general user

--- tensorflow/tf.sh
+++ tensorflow/tf.sh
@@ -3,6 +3,9 @@
 function tf() {
     docker run --runtime=nvidia \
 	   -v `realpath ~/GitHub`:/notebooks/GitHub \
+           -v /etc/group:/etc/group:ro \
+	   -v /etc/passwd:/etc/passwd:ro \
+	   -u $(id -u $USER):$(id -g $USER) \
 	   --rm \
 	   -it \
 	   -p 8888:8888 \
