7bf4db92014aed5c043419da797ce4c267168b6c
hamling-ling <nobubijinesu@gmail.com>
2019-02-24 17:40:07 +0900

    remove unused script

--- tensorflow/tf-devel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker run --runtime=nvidia -it \
-       -w /tensorflow \
-       -v $PWD:/mnt \
-       -e HOST_PERMS="$(id -u):$(id -g)" \
-       tensorflow/tensorflow:nightly-devel-gpu-py3 bash
-
-
