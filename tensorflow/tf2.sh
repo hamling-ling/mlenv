34b5c12fa900ace1c25bd3b694c8d23cb2b3574f
hamling-ling <nobubijinesu@gmail.com>
2020-03-26 23:11:11 +0900

    run jupyter notebook w/o auth for tensorflow2

--- tensorflow/tf2.sh
+++ tensorflow/tf2.sh
@@ -9,11 +9,10 @@ fi
 
 function tf() {
     docker run --runtime=nvidia \
-	   -v `realpath ~/GitHub`:/notebooks/GitHub \
            -v /etc/group:/etc/group:ro \
 	   -v /etc/passwd:/etc/passwd:ro \
-	    -v $HOME/GitHub:$HOME/GitHub -e "HOME=$HOME" \
-	   $USER_OPT $USER_PARAM \
+	   -v $HOME/GitHub:$HOME/GitHub -e "HOME=$HOME" -e "GRANT_SUDO=yes"\
+	    $USER_OPT $USER_PARAM \
 	   --rm \
 	   -it \
 	   -p 8888:8888 \
