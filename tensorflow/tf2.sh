6f29e3a313e57fb6f6ab9aca87218ced403a447a
hamling-ling <nobubijinesu@gmail.com>
2020-03-26 19:11:20 +0900

    created tensorflow1 and 2 docker files and running scripts

---
+++ tensorflow/tf2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USER_OPT=
+USER_PARAM=
+if [ "$1" != "--root" ]; then
+    USER_OPT=-u
+    USER_PARAM=$(id -u $USER):$(id -g $USER)
+fi
+
+function tf() {
+    docker run --runtime=nvidia \
+	   -v `realpath ~/GitHub`:/notebooks/GitHub \
+           -v /etc/group:/etc/group:ro \
+	   -v /etc/passwd:/etc/passwd:ro \
+	    -v $HOME/GitHub:$HOME/GitHub -e "HOME=$HOME" \
+	   $USER_OPT $USER_PARAM \
+	   --rm \
+	   -it \
+	   -p 8888:8888 \
+	   -p 6006:6006 \
+	   tensor2-plus bash
+}
+
+tf
